e4a2e05813dfb190c411d3c8b47d0677a569dbca
Thatoyaka Dimakatso Malope <dimakatso.malopemojapelo@gmail.com>
2026-05-03 23:41:58 +0200

    Add files via upload

---
+++ pipeline/spark_session.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+from delta import configure_spark_with_delta_pip
+
+
+def get_spark(app_name: str):
+    builder = (
+        SparkSession.builder
+        .appName(app_name)
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        .config("spark.sql.shuffle.partitions", "4")
+    )
+
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+    return spark
